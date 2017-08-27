a9a782601cae15e5dba7d857c4d1cf15b12fe4c0
张弓 <sz9214e@qq.com>
2017-08-27 14:34:35 +0800

    增加对js调用的支持

--- app/src/main/java/cn/cerc/summer/android/basis/utils/CallLoginByAccount.java
+++ app/src/main/java/cn/cerc/summer/android/basis/utils/CallLoginByAccount.java
@@ -1,8 +1,10 @@
 package cn.cerc.summer.android.basis.utils;
 
 import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
 
 import cn.cerc.summer.android.basis.forms.JavaScriptService;
+import cn.cerc.summer.android.parts.login.FrmLoginByAccount;
 
 /**
  * Created by Jason<sz9214e@qq.com> on 2017/8/9.
@@ -11,7 +13,7 @@ import cn.cerc.summer.android.basis.forms.JavaScriptService;
 public class CallLoginByAccount implements JavaScriptService {
     @Override
     public String execute(Context context, String dataIn) {
-        //TODO: 此功能还未准备好
-        throw new RuntimeException("此功能还未准备好");
+        FrmLoginByAccount.startForm((AppCompatActivity) context);
+        return "true";
     }
 }
