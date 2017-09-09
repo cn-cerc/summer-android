2f44c8a4d4a78a96d63d5fdf5301525b60f39690
张弓 <sz9214e@qq.com>
2017-09-09 15:50:44 +0800

    增加二维码生成、扫描识别的功能

--- app/src/main/java/cn/cerc/summer/android/basis/utils/ScanBarcode.java
+++ app/src/main/java/cn/cerc/summer/android/basis/utils/ScanBarcode.java
@@ -1,10 +1,12 @@
 package cn.cerc.summer.android.basis.utils;
 
 import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
 
 import org.json.JSONObject;
 
 import cn.cerc.summer.android.basis.forms.JavaScriptService;
+import cn.cerc.summer.android.parts.barcode.FrmScanBarcode;
 
 /**
  * Created by Jason<sz9214e@qq.com> on 2017/8/9.
@@ -13,7 +15,7 @@ import cn.cerc.summer.android.basis.forms.JavaScriptService;
 public class ScanBarcode implements JavaScriptService {
     @Override
     public String execute(Context context, JSONObject request) throws Exception {
-        //TODO: 此功能还未准备好
-        throw new RuntimeException("此功能还未准备好");
+        FrmScanBarcode.startForm((AppCompatActivity) context);
+        return "true";
     }
 }
