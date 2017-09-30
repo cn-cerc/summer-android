0c7f73af6adcbfed0e4bd27c4336e000a46f8533
张弓 <sz9214e@qq.com>
2017-09-30 17:24:56 +0800

    改进通话与读取通讯记录功能

--- app/src/main/java/cn/cerc/summer/android/basis/forms/JavaScriptProxy.java
+++ app/src/main/java/cn/cerc/summer/android/basis/forms/JavaScriptProxy.java
@@ -17,7 +17,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import cn.cerc.summer.android.basis.core.MyApp;
-import cn.cerc.summer.android.basis.utils.GetPhoneTime;
+import cn.cerc.summer.android.basis.utils.GetTalkLength;
 import cn.cerc.summer.android.basis.utils.CallLoginByAccount;
 import cn.cerc.summer.android.basis.utils.CallLoginByPhone;
 import cn.cerc.summer.android.basis.utils.CallPhoneNumber;
@@ -54,7 +54,7 @@ public class JavaScriptProxy extends Object {
 
     static {
         services.put(CallPhoneNumber.class, "拔打指定的电话号码");
-        services.put(GetPhoneTime.class, "取得最近一次电话拨出时长");
+        services.put(GetTalkLength.class, "取得最近一次电话拨出时长");
         //
         services.put(CallLoginByAccount.class, "使用标准的帐号、密码进行登录");
         services.put(CallLoginByPhone.class, "使用手机号及验证码进行登录");
--- app/src/main/java/cn/cerc/summer/android/basis/utils/GetTalkLength.java
+++ app/src/main/java/cn/cerc/summer/android/basis/utils/GetTalkLength.java
@@ -18,7 +18,7 @@ import cn.cerc.summer.android.basis.forms.JavaScriptService;
  * Created by Hasee on 2017/9/28.
  */
 
-public class GetPhoneTime implements JavaScriptService {
+public class GetTalkLength implements JavaScriptService {
 
     @Override
     public String execute(Context context, JSONObject request) throws Exception {
@@ -26,18 +26,14 @@ public class GetPhoneTime implements JavaScriptService {
 
         TelephonyManager tm = (TelephonyManager) context
                 .getSystemService(Service.TELEPHONY_SERVICE);
-        JSONObject json = new JSONObject();
-        json.put("result", false);
 
         if (tm.getCallState() != TelephonyManager.CALL_STATE_IDLE) {
-            json.put("message", "当前手机不是空闲状态");
-            return json.toString();
+            throw new RuntimeException("当前手机不是空闲状态");
         }
 
         if (ActivityCompat.checkSelfPermission(frmMain, Manifest.permission.READ_CALL_LOG)
                 != PackageManager.PERMISSION_GRANTED) {
-            json.put("message", "没有拨打电话权限");
-            return json.toString();
+            throw new RuntimeException("没有拨打电话权限");
         }
 
         Cursor cursor = frmMain.getContentResolver().query(CallLog.Calls.CONTENT_URI,
@@ -51,21 +47,20 @@ public class GetPhoneTime implements JavaScriptService {
         long outgoing = 0L;
         int count = 0;
         if (!hasRecord) {
-            json.put("message", "没有通话记录");
-            return json.toString();
+            throw new RuntimeException("没有通话记录");
         }
 
         int type = cursor.getInt(cursor.getColumnIndex(CallLog.Calls.TYPE));
         String phoneNumber = cursor.getString(cursor.getColumnIndex(CallLog.Calls.NUMBER));
         if (type != CallLog.Calls.OUTGOING_TYPE) {
-            json.put("message", "该通话记录不是呼出记录");
-            return json.toString();
+            throw new RuntimeException("该通话记录不是呼出记录");
         }
 
         long callTime = cursor.getLong(cursor.getColumnIndex(CallLog.Calls.DURATION));
+        JSONObject json = new JSONObject();
+        json.put("result", true);
         json.put("message", "ok");
         json.put("phoneNumber", phoneNumber);
-        json.put("result", true);
         json.put("timeLength", callTime);
         return json.toString();
     }
