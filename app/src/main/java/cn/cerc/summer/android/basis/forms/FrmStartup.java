c4eb8f006713b67b3002559382ed70b3c2d28f2b
张弓 <sz9214e@qq.com>
2017-10-01 23:36:42 +0800

    读取后台版本号

--- app/src/main/java/cn/cerc/summer/android/basis/forms/FrmStartup.java
+++ app/src/main/java/cn/cerc/summer/android/basis/forms/FrmStartup.java
@@ -6,27 +6,56 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
+import android.widget.Toast;
 
 import com.mimrc.vine.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import cn.cerc.summer.android.basis.core.MyApp;
+import cn.cerc.summer.android.basis.db.HttpClient;
+import cn.cerc.summer.android.basis.db.RemoteForm;
+
+import static com.google.android.gms.common.zze.rf;
 
 public class FrmStartup extends AppCompatActivity {
     private FrmStartup instince;
     private Timer timer = new Timer();
+    private int MSG_CLIENT = 1;
     private Handler handler = new Handler(){
 
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            if(msg.what == 1){
-                Intent intent = new Intent();
-                intent.setClass(instince, FrmMain.class);
-                instince.startActivity(intent);
-                finish();
+            if(msg.what == MSG_CLIENT){
+                String str = (String) msg.obj;
+                JSONObject json = null;
+                try {
+                    json = new JSONObject(str);
+                    if(json.has("result")){
+                        if(json.getBoolean("result")){
+                            String value = json.getString("appVersion");
+                            MyApp.getInstance().setAppVersion(value);
+                            //启动主窗口
+                            Intent intent = new Intent();
+                            intent.setClass(instince, FrmMain.class);
+                            instince.startActivity(intent);
+                            finish();
+                        }else{
+                            Toast.makeText(instince, json.getString("message"), Toast.LENGTH_LONG).show();
+                        }
+                    }else{
+                        Toast.makeText(instince, "无法取得后台服务，请稍后再试！", Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(instince, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
             }
         }
     };
@@ -37,14 +66,30 @@ public class FrmStartup extends AppCompatActivity {
         setContentView(R.layout.activity_frm_startup);
         instince = this;
 
+        /*
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
                 Message msg = new Message();
-                msg.what = 1;
+                msg.what = MSG_CLIENT;
                 handler.sendMessage(msg);
             }
         }, 3000);
+        */
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpClient client = new HttpClient(MyApp.getFormUrl("install.client"));
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("client", "android");
+                String response = client.post(params);
+                Message msg = new Message();
+                msg.what = MSG_CLIENT;
+                msg.obj = response;
+                handler.sendMessage(msg);
+            }
+        }).start();
 
         TelephonyManager TelephonyMgr = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
         MyApp.IMEI = TelephonyMgr.getDeviceId();
