2f0e16a4be8acf733d4e8145557616d080d46819
juewulingluan <yangtaiyu@163.com>
2017-10-11 10:53:44 +0800

    修改设置菜单

--- app/src/main/java/cn/cerc/summer/android/basis/utils/SetMenuList.java
+++ app/src/main/java/cn/cerc/summer/android/basis/utils/SetMenuList.java
@@ -18,47 +18,31 @@ import cn.cerc.summer.android.basis.forms.JavaScriptService;
  */
 
 public class SetMenuList implements JavaScriptService {
-    private List<MainTitleMenu> list;  //临时存储数据集合
 
     @Override
     public String execute(Context context, JSONObject request) throws Exception {
-        if (!request.has("menus")) {
-            return "没有菜单项";
-        }
         FrmMain frmMain = (FrmMain) context;
-        list = new ArrayList<MainTitleMenu>();
-        for (int d = 0; d < frmMain.maxListl.size(); d++) {
-            if (2 == frmMain.maxListl.get(d).getNum()) {
-                list.add(frmMain.maxListl.get(d));
+        if (request.has("menus")) {
+            //标题栏菜单数据
+            JSONObject menus = request.getJSONObject("menus");
+            Iterator it = menus.keys();
+            while (it.hasNext()) {
+                String key = (String) it.next();
+                String value = menus.getString(key);
+                Log.i("SetMenuList", key + "-" + value);
+                frmMain.CatalogTitleWebView(key, value);
             }
         }
-        frmMain.maxListl.removeAll(list);
-        list.clear();
-        for (int i = 0; i < frmMain.mTitleList1.size(); i++) {
-            if (2 == frmMain.mTitleList1.get(i).getNum()) {
-                list.add(frmMain.mTitleList1.get(i));
+        if (request.has("history")) {
+            //右侧菜单数据
+            JSONObject history = request.getJSONObject("history");
+            Iterator itTitle = history.keys();
+            while (itTitle.hasNext()) {
+                String key = (String) itTitle.next();
+                String value = history.getString(key);
+                Log.i("SetMenuList——————", key + "-" + value);
+                frmMain.CatalogWebView(key, value);
             }
-        }
-        frmMain.mTitleList1.removeAll(list);
-        list.clear();
-        //右侧菜单数据
-        JSONObject objTitle = request.getJSONObject("history");
-        Iterator itTitle = objTitle.keys();
-        while (itTitle.hasNext()) {
-            String key = (String) itTitle.next();
-            String value = objTitle.getString(key);
-            Log.i("SetMenuList——————", key + "-" + value);
-            frmMain.CatalogWebView(key, value);
-        }
-
-        //标题栏菜单数据
-        JSONObject objSetup = request.getJSONObject("menus");
-        Iterator it = objSetup.keys();
-        while (it.hasNext()) {
-            String key = (String) it.next();
-            String value = objSetup.getString(key);
-            Log.i("SetMenuList", key + "-" + value);
-            frmMain.CatalogTitleWebView(key, value);
         }
         return "ok";
     }
