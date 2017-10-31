3df90cc8f72e9e1671c60773453282f1dd562ab1
黄荣君 <l1091462907@gmail.com>
2017-10-31 13:49:57 +0800

    修改 ScanBarcode 代码

--- app/src/main/java/cn/cerc/summer/android/services/ScanBarcode.java
+++ app/src/main/java/cn/cerc/summer/android/services/ScanBarcode.java
@@ -1,6 +1,10 @@
 package cn.cerc.summer.android.services;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import org.json.JSONObject;
@@ -18,17 +22,24 @@ public class ScanBarcode implements JavaScriptService {
         if (!request.has("type")) {
             return "没有操作类型";
         }
-        //操作类型：1、调后台；2、数据为 url ,直接跳转、3、回调js方法
+
+        // 操作类型：1、调后台；2、数据为 url ,直接跳转、3、回调js方法
         int type = request.getInt("type");
-        if(type == 3 && !request.has("jsFun")){
+        if (type == 3 && !request.has("jsFun")) {
             return "没有回调js方法";
         }
+
         //后台 forms
         String forms = request.getString("forms");
         //js方法
         String jsFun = request.getString("jsFun");
-
-        FrmScanBarcode.startForm((AppCompatActivity) context, type, forms, jsFun);
-        return "true";
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            FrmScanBarcode.startForm((AppCompatActivity) context, type, forms, jsFun);
+            return "true";
+        } else {
+            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.CAMERA}, 35);
+            return "false";
+        }
     }
 }
