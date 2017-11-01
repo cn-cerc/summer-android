5d5ec90796ed71dd342e59de50d9ad4110f380fc
lzq <805571245@qq.com>
2017-11-01 10:34:18 +0800

    扫一扫功能完善

--- app/src/main/java/cn/cerc/summer/android/services/ScanBarcode.java
+++ app/src/main/java/cn/cerc/summer/android/services/ScanBarcode.java
@@ -1,9 +1,7 @@
 package cn.cerc.summer.android.services;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
