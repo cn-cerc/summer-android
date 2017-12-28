96ee20afcc332bc65c86b6a38f1bd82c4d48ad6e
杨泰宇 <yangtaiyu@163.com>
2017-12-28 17:30:01 +0800

    调用外部浏览器打开url

---
+++ app/src/main/java/cn/cerc/summer/android/services/CallBrowser.java
@@ -0,0 +1,33 @@
+package cn.cerc.summer.android.services;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import org.json.JSONObject;
+
+import cn.cerc.summer.android.forms.JavaScriptService;
+
+/**
+ * Created by Administrator on 2017/12/28.
+ */
+
+public class CallBrowser  implements JavaScriptService{
+
+    @Override
+    public String execute(Context context, JSONObject request) throws Exception {
+        if(request.has("url")){
+            String url = request.getString("url");
+            if(!"".equals(url)){
+                Intent intent= new Intent();
+                intent.setAction("android.intent.action.VIEW");
+                Uri content_url = Uri.parse(url);
+                intent.setData(content_url);
+                context.startActivity(intent);
+            }
+        }else{
+            return "没有传入指定参数";
+        }
+        return "";
+    }
+}
