333dc2ec98010849d7ec0d9e45b6cf84e6195051
杨泰宇 <yangtaiyu@163.com>
2018-04-03 18:30:20 +0800

    通过js获取设备ID

--- app/src/main/java/cn/cerc/summer/android/services/GetClientId.java
+++ app/src/main/java/cn/cerc/summer/android/services/GetClientId.java
@@ -2,6 +2,7 @@ package cn.cerc.summer.android.services;
 
 import android.content.Context;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import cn.cerc.summer.android.core.MyApp;
@@ -14,7 +15,12 @@ import cn.cerc.summer.android.forms.JavaScriptService;
 public class GetClientId implements JavaScriptService {
 
     @Override
-    public String execute(Context context, JSONObject request) {
+    public String execute(Context context, JSONObject request) throws JSONException {
+        if (!request.has("_callback_")) {
+            return "没有传入指定参数";
+        }
+        String function = request.getString("_callback_");
+        MyApp.getInstance().executiveJS(function,MyApp.getInstance().getClientId());
         return MyApp.getInstance().getClientId();
     }
 }
