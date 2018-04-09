c1a2e8b05db69c320d52d1393e400855bf22b44a
杨泰宇 <yangtaiyu@163.com>
2018-04-09 12:14:04 +0800

    增加自定义推送声音

--- app/src/main/java/cn/cerc/summer/android/core/MyBroadcastReceiver.java
+++ app/src/main/java/cn/cerc/summer/android/core/MyBroadcastReceiver.java
@@ -1,9 +1,16 @@
 package cn.cerc.summer.android.core;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+
+import com.mimrc.vine.R;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,6 +18,8 @@ import org.json.JSONObject;
 import cn.cerc.summer.android.basis.RemoteForm;
 import cn.cerc.summer.android.forms.FrmMain;
 import cn.jpush.android.api.JPushInterface;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 /**
  * Created by fff on 2016/11/28.
@@ -20,6 +29,7 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
 //https://www.pgyer.com/app/install/9c2b21c08cee498109cfc02913ad6a64
 
     private static final String ACTION_NOTIFICATION_OPENED = JPushInterface.ACTION_NOTIFICATION_OPENED;
+    private static String TAG = "print";
     private final String EXTRA = "cn.jpush.android.EXTRA";
     private final String NOTIFI_ID = "cn.jpush.android.NOTIFICATION_ID";
     private final String MSG_ID = "MSG_ID";
@@ -49,16 +59,24 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
                 break;
             case Constans.NOTIFICATION_RECEIVED:
                 if (intent.hasExtra(EXTRA)) {
+                    boolean CustomSound = false;
                     try {
-                        JSONObject jsonObject = new JSONObject(intent.getStringExtra(EXTRA));
-                        if ("update".equals(jsonObject.getString("action"))) {
-                            Intent updateintent = new Intent(FrmMain.APP_UPDATA);
-                            context.sendBroadcast(updateintent);
-                        }
+                        CustomSound = processCustomMessage(context, intent.getExtras());
                     } catch (JSONException e) {
-                        Intent updateintent = new Intent(FrmMain.JSON_ERROR);
-                        context.sendBroadcast(updateintent);
                         e.printStackTrace();
+                    }
+                    if (!CustomSound) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(intent.getStringExtra(EXTRA));
+                            if ("update".equals(jsonObject.getString("action"))) {
+                                Intent updateintent = new Intent(FrmMain.APP_UPDATA);
+                                context.sendBroadcast(updateintent);
+                            }
+                        } catch (JSONException e) {
+                            Intent updateintent = new Intent(FrmMain.JSON_ERROR);
+                            context.sendBroadcast(updateintent);
+                            e.printStackTrace();
+                        }
                     }
                 }
                 break;
@@ -80,5 +98,51 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
             default:
                 break;
         }
+    }
+
+    /**
+     * 自定义推送的声音
+     *
+     * @param context
+     * @param bundle
+     */
+    private boolean processCustomMessage(Context context, Bundle bundle) throws JSONException {
+        JSONObject json = null;
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(context);
+        //这一步必须要有而且setSmallIcon也必须要，没有就会设置自定义声音不成功
+        notification.setAutoCancel(true).setSmallIcon(R.mipmap.deting);
+        String alert = bundle.getString(JPushInterface.EXTRA_EXTRA);
+        String title = bundle.getString(JPushInterface.EXTRA_NOTIFICATION_TITLE);
+        String msg = bundle.getString(JPushInterface.EXTRA_ALERT);
+        json = new JSONObject(alert);
+        if (json.has("sound")) {
+            if (json.getString("sound").equals("trade_mall")) {
+                notification.setSound(
+                        Uri.parse("android.resource://" + context.getPackageName() + "/" + R.raw.test));
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+        Intent mIntent = new Intent(context, FrmMain.class);
+        mIntent.putExtra("msgId", json.getString("msgId"));
+        mIntent.putExtras(bundle);
+        mIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, mIntent, 0);
+
+        notification.setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .setContentText(msg)
+                .setContentTitle(title.equals("") ? "title" : title)
+                .setSmallIcon(R.mipmap.deting);
+
+        int notifactionId = bundle.getInt(JPushInterface.EXTRA_NOTIFICATION_ID);
+        //bundle.get(JPushInterface.EXTRA_ALERT);推送内容
+        Log.d(TAG, "[MyReceiver] 接收到推送下来的通知的ID: " + notifactionId);
+        //最后刷新notification是必须的
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(notifactionId, notification.build());
+        return true;
     }
 }
